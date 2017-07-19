00d777e98cf499201bfbc9ce0e245caea97c1ef1
Wally Faye <wally.faye@gmail.com>
2017-07-18 23:56:24 -0400

    color display

--- color-test.sh
+++ color-test.sh
@@ -1,1 +1,8 @@
-for i in {1..255}; do printf "\e[0m$i: \e[38;05;${i}mCOLOR TEST 123\n"; done;
+for i in {1..255}; 
+	do 
+		printf "\e[38;05;${i}m\xE2\x96\x88";
+		if [ $(($i % 8)) == 0 ]
+		then
+			printf "\n";
+		fi
+done;
